8b38beb5d9f626880b155a9932f21b18f57ec1e4
Shrike <ultrahax89@gmail.com>
2022-07-25 22:00:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const video = document.createElement('video');
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+	get: function () {
+		return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+	},
+});
+var first = true;
 video.src = 'https://ia801602.us.archive.org/11/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4';
 video.autoplay = false;
 video.controls = true;
@@ -9,6 +15,8 @@ video.width = 1320; // 👈️ in px
 document.body.insertBefore(video, document.body.firstChild);
 
 setInterval(function () {
-	video.muted = 0;
-	video.play();
+	if (first && !document.querySelector('video').playing) {
+		video.play();
+		first = false;
+	}
 }, 1000);
